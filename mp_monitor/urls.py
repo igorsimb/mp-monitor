63eadaa2dfd39d6ae6ff8d49169a1a9a8ef76053
igorsimb <igor.simbirtsev@gmail.com>
2023-08-29 17:59:09 +0300

    feature(admin docs): generate admin docs

--- mp_monitor/urls.py
+++ mp_monitor/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # Django admin
+    path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
     # User management
     path("accounts/", include("allauth.urls")),
