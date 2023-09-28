bd8707e352db543bd62641ec7ce03dc9f382cf4f
igorsimb <igor.simbirtsev@gmail.com>
2023-09-28 20:55:38 +0300

    test(main): add test_main_urls
    Partially addresses https://github.com/igorsimb/mp-monitor/issues/7
    Check that a view (function or class-based)corresponds to a given URL.
    Also renamed url name from `scraped_item` to `scrape_item` for better clarity.

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from .views import ItemListView, ItemDetailView, scrape_items, create_scrape_interval_task, destroy_scrape_interval_task
 
 urlpatterns = [
@@ -6,5 +7,5 @@ urlpatterns = [
     path("scrape_interval/", create_scrape_interval_task, name="create_scrape_interval"),
     path("destroy_scrape_interval/", destroy_scrape_interval_task, name="destroy_scrape_interval"),
     path("<str:slug>/", ItemDetailView.as_view(), name="item_detail"),
-    path("scrape/<str:skus>/", scrape_items, name="scraped_item"),
+    path("scrape/<str:skus>/", scrape_items, name="scrape_item"),
 ]
---
+++ tests/main/test_main_urls.py
@@ -0,0 +1,42 @@
+import logging
+
+from django.urls import reverse, resolve
+
+from main.views import ItemListView, ItemDetailView, scrape_items, create_scrape_interval_task, \
+    destroy_scrape_interval_task
+
+logger = logging.getLogger(__name__)
+
+
+class TestMainUrls:
+    def test_item_list_url(self):
+        url = reverse("item_list")
+        logger.info("Resolving URL: %s to view class: %s", url, ItemListView)
+        assert resolve(url).func.view_class == ItemListView
+
+    def test_item_detail_url(self):
+        slug = "145823738"
+        url = reverse("item_detail", args=[slug])
+        logger.info("Resolving URL: %s to view class: %s", url, ItemDetailView)
+        assert resolve(url).func.view_class == ItemDetailView
+
+    def test_scrape_items_url(self):
+        skus = "145823738"
+        url = reverse("scrape_item", args=[skus])
+        logger.info("Resolving URL: %s to view function: %s", url, scrape_items)
+        assert resolve(url).func == scrape_items
+
+    def test_create_scrape_interval_task_url(self):
+        url = reverse("create_scrape_interval")
+        logger.info("Resolving URL: %s to view function: %s", url, create_scrape_interval_task)
+        assert resolve(url).func == create_scrape_interval_task
+
+    def test_destroy_scrape_interval_task_url(self):
+        url = reverse("destroy_scrape_interval")
+        logger.info("Resolving URL: %s to view function: %s", url, destroy_scrape_interval_task)
+        assert resolve(url).func == destroy_scrape_interval_task
+
+    def test_resolve_incorrect_url(self):
+        url = reverse("destroy_scrape_interval")
+        logger.info("Resolving incorrect URL: %s to view function: %s", url, create_scrape_interval_task)
+        assert resolve(url).func != create_scrape_interval_task, "URL resolved when it should not have!"
