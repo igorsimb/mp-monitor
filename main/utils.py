9c6ccf6c390ea3712e558658069ad196c487b26b
igorsimb <igor.simbirtsev@gmail.com>
2023-10-02 19:29:25 +0300

    test(test_main_utils): add tests for scrape_item function
    - Used pytest-mock (mocker) to isolate the tests from API in case API fails or real item's info changes (e.g. price)
    
    - Also made some changes in the `scrape_item` function itself to catch some unexpected behaviors when scraping for price

--- main/utils.py
+++ main/utils.py
@@ -6,24 +6,53 @@ from main.models import Item
 
 logger = logging.getLogger(__name__)
 
+MAX_RETRIES = 3
+
+def get_price(item: dict) -> float | None:
+    """
+    Extracts and coverts the price of an item and makes sure it's in the correct format.
+    Otherwise, returns None
+    """
+    sale_price = item.get("salePriceU")
+
+    if sale_price is not None and isinstance(sale_price, (int, float)):
+        try:
+            price = float(sale_price) / 100
+        except (ValueError, TypeError):
+            price = None
+    else:
+        price = None
+    return price
+
 
 def scrape_item(sku):
-    url = f"https://card.wb.ru/cards/detail?appType=1&curr=rub&nm={sku}"
+    # https://card.wb.ru/cards/detail?appType=1&curr=rub&nm={sku}
+    url = httpx.URL("https://card.wb.ru/cards/detail", params={'appType': 1, 'curr': 'rub', 'nm': sku})
+    retry_count = 0
+    data = {}
 
-    response = httpx.get(url)
-    data = response.json()
+    # in case of a server error, retry the request up to 3 times
+    while retry_count < MAX_RETRIES:
+        try:
+            response = httpx.get(url, timeout=15)
+            response.raise_for_status()
+            data = response.json()
+            break
+        except httpx.HTTPError as e:
+            logger.error(f"HTTP error occurred: {e}")
+            retry_count += 1
 
     item = data.get("data", {}).get("products")[0]
 
     name = item.get("name")
     sku = item.get("id")
-    price = float(item.get("salePriceU")) / 100 if item.get("salePriceU") else None
+    price = get_price(item)
     image = item.get("image")
     category = item.get("category")
     brand = item.get("brand")
     seller_name = item.get("brand")
-    rating = float(item.get("rating"))
-    num_reviews = int(item.get("feedbacks"))
+    rating = float(item.get("rating")) if item.get("rating") is not None else None
+    num_reviews = int(item.get("feedbacks")) if item.get("feedbacks") is not None else None
 
     if price is None:
         logger.error(f"Could not find salePriceU for sku {sku}")
--- tests/main/test_main_utils.py
+++ tests/main/test_main_utils.py
@@ -1,17 +1,19 @@
 import logging
 
+import httpx
 import pytest
 from django.contrib.auth import get_user_model
 
 from main.models import Item, Tenant
-from main.utils import uncheck_all_boxes
+from main.utils import uncheck_all_boxes, scrape_item
+
+pytestmark = [pytest.mark.django_db]
 
 logger = logging.getLogger(__name__)
 
 User = get_user_model()
 
 
-@pytest.mark.django_db
 class TestUncheckAllBoxes:
     @pytest.fixture
     def user(self):
@@ -27,8 +29,10 @@ class TestUncheckAllBoxes:
         number_of_items = 3
 
         logger.info("Creating %s items for tenant '%s'", number_of_items, tenant)
-        items = [Item.objects.create(tenant=tenant, name=f"Item {i}", sku=f"1234{i}", parser_active=True)
-                 for i in range(1, number_of_items + 1)]
+        items = [
+            Item.objects.create(tenant=tenant, name=f"Item {i}", sku=f"1234{i}", parser_active=True)
+            for i in range(1, number_of_items + 1)
+        ]
 
         request.user = user
         logger.info("Calling uncheck_all_boxes() for tenant '%s'", tenant)
@@ -100,5 +104,109 @@ class TestUncheckAllBoxes:
 
         inactive_items = Item.objects.filter(tenant=tenant, parser_active=False)
         for item in inactive_items:
-            assert not item.parser_active, \
-                f"Item {item.name} parser_active should be False but it is {item.parser_active}"
+            assert (
+                not item.parser_active
+            ), f"Item {item.name} parser_active should be False but it is {item.parser_active}"
+
+
+class TestScrapeItem:
+    # Use pytest-mock (mocker) to isolate the tests from API in case API fails or real item's info changes (e.g. price)
+    @pytest.fixture(autouse=True)
+    def setup(self, mocker):
+        self.sku = "12345"
+        self.mock_response = httpx.Response(
+            200,
+            json={
+                "data": {
+                    "products": [
+                        {
+                            "name": "Test Item",
+                            "id": self.sku,
+                            "salePriceU": 10000,
+                            "image": "test.jpg",
+                            "category": "Test Category",
+                            "brand": "Test Brand",
+                            "seller_name": "Test Brand",
+                            "rating": 4.5,
+                            "feedbacks": 10,
+                        }
+                    ]
+                }
+            },
+        )
+
+        self.mock_response.request = httpx.Request(
+            method="GET", url=f"https://card.wb.ru/cards/detail?appType=1&curr=rub&nm={self.sku}"
+        )
+
+        mocker.patch("httpx.get", return_value=self.mock_response)
+
+    def test_retrieve_data_from_mock_api(self):
+        logger.info("Calling scrape_item() with a mock SKU (%s)", self.sku)
+        result = scrape_item(self.sku)
+
+        logger.info("Checking that the result is the expected parsed item dictionary")
+        assert result == {
+            "name": "Test Item",
+            "sku": self.sku,
+            "price": 100.0,
+            "image": "test.jpg",
+            "category": "Test Category",
+            "brand": "Test Brand",
+            "seller_name": "Test Brand",
+            "rating": 4.5,
+            "num_reviews": 10,
+        }
+
+    def test_retry_request_on_http_error(self, mocker):
+        mocker.patch("httpx.get", side_effect=[
+            httpx.HTTPError(message="Expected Error"),
+            httpx.HTTPError(message="Expected Error"),
+            self.mock_response
+        ])
+
+        logger.info("Calling scrape_item() with a mock SKU (%s)", self.sku)
+        result = scrape_item(self.sku)
+
+        logger.info("Checking that the result is the expected parsed item dictionary")
+        assert result == {
+            "name": "Test Item",
+            "sku": self.sku,
+            "price": 100,
+            "image": "test.jpg",
+            "category": "Test Category",
+            "brand": "Test Brand",
+            "seller_name": "Test Brand",
+            "rating": 4.5,
+            "num_reviews": 10,
+        }
+
+    def test_return_none_for_missing_price(self):
+        response_data = self.mock_response.json()
+
+        logger.info("Removing the salePriceU field")
+        del response_data["data"]["products"][0]["salePriceU"]
+
+        logger.info("Returning the modified response_data dictionary")
+        self.mock_response.json = lambda: response_data
+
+        logger.info("Calling scrape_item() with a mock SKU (%s)", self.sku)
+        result = scrape_item(self.sku)
+
+        logger.info("Checking that the resulting price is None")
+        assert result["price"] is None
+
+    def test_return_none_for_invalid_price_format(self):
+        response_data = self.mock_response.json()
+
+        logger.info("Assigning invalid price format to the salePriceU field")
+        response_data["data"]["products"][0]["salePriceU"] = "invalidprice"
+
+        logger.info("Returning the modified response_data dictionary")
+        self.mock_response.json = lambda: response_data
+
+        logger.info("Calling scrape_item() with a mock SKU (%s)", self.sku)
+        result = scrape_item(self.sku)
+
+        logger.info("Checking that the resulting price is None")
+        assert result["price"] is None
