c48ab85dfe8a5ee871f0228aef7cb0ab343fce22
igorsimb <igor.simbirtsev@gmail.com>
2024-02-07 18:29:53 +0300

    security(urls): change url path for admin
    For a slight security improvement.

--- mp_monitor/urls.py
+++ mp_monitor/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     # Django admin
-    path("admin/", admin.site.urls),
+    path("definitely-not-admin/", admin.site.urls),
     # User management
     path("accounts/", include("allauth.urls")),
     # Local apps
