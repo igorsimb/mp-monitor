c9e9bf80965895dc691ab80cd37c360dcf570ed4
igorsimb <igor.simbirtsev@gmail.com>
2024-04-10 21:20:31 +0300

    fix(login): re-design for login page
    Fixes #65

--- mp_monitor/urls.py
+++ mp_monitor/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from accounts.views import logout_view
+
 urlpatterns = [
     # Django admin
     path("definitely-not-admin/", admin.site.urls),
     # User management
+    path("accounts/logout/", logout_view, name="logout"),
     path("accounts/", include("allauth.urls")),
     # Local apps
     path("", include("main.urls")),
