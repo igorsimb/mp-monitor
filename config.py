1ef7ca44b874497b0b656c730ecaf992dab9287e
igorsimb <igor.simbirtsev@gmail.com>
2024-07-23 15:16:55 +0300

    feat(config): create a dict with default quotas for each plan
    Fixes #169

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ MAX_ITEMS_ON_SCREEN = 10  # used in the messages when many items are scraped
 MAX_RETRIES = 10
 
 
-class PaymentPlan(Enum):
+class PlanType(Enum):
     # can be accessed like so: PaymentPlan.FREE, etc
     FREE = "Free"
     BUSINESS = "Business"
@@ -22,22 +22,22 @@ class PaymentPlan(Enum):
 
 
 DEFAULT_QUOTAS = {
-    PaymentPlan.FREE: {
+    PlanType.FREE: {
         "total_hours_allowed": HOURS_ALLOWED,
         "skus_limit": 50,
         "parse_units_limit": 5000,
     },
-    PaymentPlan.BUSINESS: {
+    PlanType.BUSINESS: {
         "total_hours_allowed": HOURS_ALLOWED,
         "skus_limit": 500,
         "parse_units_limit": 35_000,
     },
-    PaymentPlan.PRO: {
+    PlanType.PRO: {
         "total_hours_allowed": HOURS_ALLOWED,
         "skus_limit": 1000,
         "parse_units_limit": 100_000,
     },
-    PaymentPlan.CORPORATE: {
+    PlanType.CORPORATE: {
         "total_hours_allowed": HOURS_ALLOWED,
         "skus_limit": 5000,
         "parse_units_limit": 500_000,
