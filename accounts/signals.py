8ad0dd8ef9b54a04ef98b80a655a4e8bb7f10a2c
igorsimb <igor.simbirtsev@gmail.com>
2024-08-30 20:51:42 +0300

    feature(signals): add signals for assigning default plan and quota
    Scenario:
    User creates a new tenant
    Given a new tenant
    When the tenant is created
    Then the default plan is assigned to the tenant
    And the default quota is assigned to the tenant
    
    Also, created tests for the signals

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,6 +1,21 @@
-# from django.db import transaction
-# from django.db.models.signals import post_save, pre_save
-# from django.dispatch import receiver
-#
-# from accounts.models import Tenant, TenantQuota
+from django.db import transaction
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+from accounts.models import Tenant, TenantQuota, PaymentPlan
+
+
+@receiver(post_save, sender=Tenant)
+@transaction.atomic
+def assign_default_plan(sender, instance, created, **kwargs):
+    if created:
+        instance.payment_plan = PaymentPlan.get_default_payment_plan()
+        instance.save()
+
+
+@receiver(post_save, sender=Tenant)
+@transaction.atomic
+def assign_default_plan_quota(sender, instance, created, **kwargs):
+    if created:
+        instance.quota = TenantQuota.get_default_quota()
+        instance.save()
---
+++ tests/accounts/test_accounts_signals.py
@@ -0,0 +1,37 @@
+from accounts.models import TenantQuota, PaymentPlan
+from config import PlanType, DEFAULT_QUOTAS
+from tests.factories import TenantFactory
+
+
+def test_default_plan_is_assigned() -> None:
+    """Test that the default payment plan is assigned to a new tenant."""
+    tenant = TenantFactory()
+    free_plan = PaymentPlan.get_default_payment_plan()
+    assert tenant.payment_plan == free_plan
+
+
+def test_default_plan_is_free() -> None:
+    """Test that the default payment plan is FREE."""
+    tenant = TenantFactory()
+    assert tenant.payment_plan.name == PaymentPlan.PlanName.FREE.value
+
+
+def test_default_plan_quota_is_assigned() -> None:
+    """Test that the default quota is assigned to a new tenant."""
+    tenant = TenantFactory()
+    free_plan_quota = TenantQuota.get_default_quota()
+    assert tenant.quota == free_plan_quota
+
+
+def test_default_plan_quota_is_correct() -> None:
+    """Test that the default quota is the same as in config.py file."""
+    tenant = TenantFactory()
+    assert tenant.quota.skus_limit == DEFAULT_QUOTAS[PlanType.FREE]["skus_limit"]
+    assert tenant.quota.parse_units_limit == DEFAULT_QUOTAS[PlanType.FREE]["parse_units_limit"]
+
+
+def test_not_default_plan_quota_is_not_correct() -> None:
+    """Test that the default quota is not BUSINESS plan quota from config.py file."""
+    tenant = TenantFactory()
+    assert tenant.quota.skus_limit != DEFAULT_QUOTAS[PlanType.BUSINESS]["skus_limit"]
+    assert tenant.quota.parse_units_limit != DEFAULT_QUOTAS[PlanType.BUSINESS]["parse_units_limit"]
