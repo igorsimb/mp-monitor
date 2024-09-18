f0f267b34aaa0be8b66c87052a52440bad966e9c
igorsimb <igor.simbirtsev@gmail.com>
2024-09-18 14:40:06 +0300

    fix(settings.py): add EMAIL_PORT to env variables
    netganels.ru does not support remote connections to outside mail servers (gmail in this case), we need to use env vars to set up their email server
    https://www.netangels.ru/support/e-mail/mail/

--- mp_monitor/settings.py
+++ mp_monitor/settings.py
@@ -172,15 +172,16 @@ AUTH_USER_MODEL = "accounts.User"
 # Email
 if LOCAL_DEVELOPMENT:
     # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_USE_TLS = False
 else:
-    EMAIL_BACKEND = "notifier.email.EmailBackend"
+    # EMAIL_BACKEND = "notifier.email.EmailBackend"
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_USE_TLS = True
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = 587
+EMAIL_PORT = env("EMAIL_PORT")
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
 
 
