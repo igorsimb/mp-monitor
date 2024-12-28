6af342bda480b0915960ec6b48ec120d397d43a9
igorsimb <igor.simbirtsev@gmail.com>
2024-12-29 02:24:04 +0300

    fix(admin): register Profile and add MEDIA settings

--- mp_monitor/settings.py
+++ mp_monitor/settings.py
@@ -163,6 +163,9 @@ if LOCAL_DEVELOPMENT:
 else:
     STATIC_ROOT = env("STATIC_ROOT")
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
