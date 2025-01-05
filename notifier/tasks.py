a27a83c5c0d8058e413d30f47ac3db72a438c61a
igorsimb <igor.simbirtsev@gmail.com>
2025-01-05 17:33:30 +0300

    improvement(email): add txt version to price change email
    Also, added user.profile.name as the context.

--- notifier/tasks.py
+++ notifier/tasks.py
@@ -1,6 +1,7 @@
 import logging
+
 from celery import shared_task
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 from accounts.models import Tenant
@@ -9,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 @shared_task
-def send_price_change_email(tenant_id, items_data):
+def send_price_change_email(tenant_id: int, items_data: list[dict]) -> None:
     """
     Send an email notification to all users of a tenant about the price change of their items.
 
@@ -18,18 +19,25 @@ def send_price_change_email(tenant_id, items_data):
         items_data (list): A list of dictionaries containing the data for the items that have changed their prices.
     """
     tenant = Tenant.objects.get(id=tenant_id)
-    email_subject = "Цена товаров изменилась"
+    user = tenant.users.first()
+    email_subject = f"Цена товаров ({len(items_data)}) изменилась"
     email_recipients = [user.email for user in tenant.users.all()]
 
-    # Prepare email content using a template
-    email_content = render_to_string(
-        "notifier/emails/price_change_notification.html", {"tenant": tenant, "items": items_data}
+    text_content = render_to_string(
+        "notifier/emails/price_change_notification.txt", context={"user": user.profile.name, "items": items_data}
     )
+
+    html_content = render_to_string(
+        "notifier/emails/price_change_notification.html", context={"user": user.profile.name, "items": items_data}
+    )
+
     logger.info(f"Sending email to {email_recipients} with subject '{email_subject}'")
-    send_mail(
-        subject=email_subject,
-        message="",
-        from_email="info@mpmonitor.ru",
-        recipient_list=email_recipients,
-        html_message=email_content,
+    msg = EmailMultiAlternatives(
+        email_subject,
+        text_content,
+        "info@mpmonitor.ru",
+        email_recipients,
     )
+
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
